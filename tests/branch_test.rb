d40f16ba11987d59786ce37976447ee75dbfa1f1
Peter Camp <Pete.f.Camp@gmail.com>
2016-04-03 12:20:43 -0500

    test

--- tests/branch_test.rb
+++ tests/branch_test.rb
@@ -1,7 +1,26 @@
-  require "test_helper"
-  require_relative "../lib/branch.rb"
-  
-b = Branch.new(branch_name: "west", address: "123 harrison", phone_number: "1234567890") 
+require_relative "test helper"
+
+class BranchTest < MiniTest::test_helper
+
+
+
+
+end 
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
