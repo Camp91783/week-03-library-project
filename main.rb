0bc391f76e642332bdc272dc99ffc79337c4b8e7
Peter Camp <Pete.f.Camp@gmail.com>
2016-04-10 12:58:00 -0500

    update before break, resume on creating new branch

--- main.rb
+++ main.rb
@@ -20,14 +20,29 @@ end
  #branches index
  	get '/branches/index' do
  	  @branches = Branch.all
- 	  erb :Branches_index
+ 	  erb :branches_index
  	end
 
  	#branch show
- get 'branch/:id' do
+ get '/branch/:id' do
  	  @branch = Branch.find_by_id(params['id'])
  	  erb :branch_show
  end
+
+ get '/branch/new' do
+   @branch = Branch.new
+   erb :branch_new
+ end
+ 
+ post '/branches/new' do
+   @branch = Branch.new(params)
+   if @branch.save
+     redirect to("/branches")
+   else
+     erb :branch_new
+   end
+ end
+ 
 
 
 
